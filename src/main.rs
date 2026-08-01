6474fb7ac31bbe840f7c30537a8963469ac78d26
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-01 19:20:53 +0200

    feat: show prompt and read command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        prompt();
+        let command = get_command();
+        print!("{}", &command);
+    }
+}
+
+fn prompt() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+}
+
+fn get_command() -> String {
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).unwrap();
+    command
 }
