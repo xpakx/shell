9c819ca36a340feba9f43d6c4a06b1d391fe682a
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-01 19:28:21 +0200

    feat: add basic split of command

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
     loop {
         prompt();
         let command = get_command();
-        print!("{}", &command);
+        let (command, args) = parse_command(&command);
+        println!("{}, {:?}", &command, &args);
     }
 }
 
@@ -17,4 +18,34 @@ fn get_command() -> String {
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
     command
+}
+
+fn parse_command(command: &str) -> (String, Vec<String>) {
+    let mut args: Vec<String> = Vec::new();
+    let mut last = ' ';
+    let mut arg = String::with_capacity(command.len());
+    let mut chars = command.trim().chars();
+
+    while let Some(c) = chars.next() {
+        match c {
+            ' ' => match last {
+                ' ' => (),
+                _ => {
+                    args.push(arg);
+                    arg = String::with_capacity(command.len());
+                }
+            },
+            _ => arg.push(c),
+        }
+        last = c;
+    }
+    if arg != "" {
+        args.push(arg);
+    }
+    if args.is_empty() {
+       return (String::new(), args)
+    }
+
+    let command = args.remove(0);
+    (command, args)
 }
