93c5aedc227b182cefabe0227b061f27eb19d5ee
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-04 20:15:16 +0200

    refactor: extract parsing to new file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use core::writeln;
 use std::{io::{self, Write, BufWriter}, process::exit};
-use std::path::{PathBuf, Path};
+use std::path::Path;
 use std::env;
-use std::fs::{self, OpenOptions};
-use std::os::unix::fs::PermissionsExt;
+use std::fs::OpenOptions;
 use std::process::Command;
 use rustyline::{self, history::DefaultHistory};
 
 mod readline;
 use readline::CommandHelper;
+
+mod parser;
+use parser::{parse_command, Cmd, Builtin, Executable};
 
 fn main() {
     let rl_config = rustyline::Config::builder()
@@ -32,87 +34,6 @@ fn get_command(rl: &mut rustyline::Editor<CommandHelper, DefaultHistory>) -> Str
         Ok(line) => line,
         Err(_) => String::new(),
     }
-}
-
-
-enum ParseMode {
-    Normal,
-    SingleQuote,
-    DoubleQuote,
-}
-
-
-fn parse_command(command: &str) -> (Cmd, Vec<String>) {
-    let home = std::env::home_dir()
-        .map(|path| path.to_string_lossy().into_owned())
-        .unwrap_or_default();
-
-    let mut args: Vec<String> = Vec::new();
-    let mut last = ' ';
-    let mut arg = String::with_capacity(command.len());
-    let mut chars = command.trim().chars().peekable();
-    let mut mode = ParseMode::Normal;
-
-    while let Some(c) = chars.next() {
-        match mode {
-            ParseMode::Normal => match c {
-                ' ' => match last {
-                    ' ' => (),
-                    _ => {
-                        if !arg.is_empty() {
-                            args.push(arg);
-                            arg = String::new();
-                        }
-                    }
-                },
-                '\'' => mode = ParseMode::SingleQuote,
-                '"' => mode = ParseMode::DoubleQuote,
-                '\\' => if let Some(next) = chars.next() {
-                    arg.push(next);
-                },
-                '~' => arg.push_str(&home),
-                '>' => {
-                    let have_io_number =  arg == "1" || arg == "2";
-                    if !have_io_number && !arg.is_empty() {
-                        args.push(arg);
-                        arg = String::with_capacity(command.len());
-                    }
-                    arg.push(c);
-                    if let Some(&next) = chars.peek() {
-                        if next == '>' {
-                            chars.next();
-                            arg.push(c);
-                        }
-                    }
-                    args.push(arg);
-                    arg = String::with_capacity(command.len());
-                },
-                _ => arg.push(c),
-            },
-            ParseMode::SingleQuote => match c {
-                '\'' => mode = ParseMode::Normal,
-                _ => arg.push(c),
-            }
-            ParseMode::DoubleQuote => match c {
-                '"' => mode = ParseMode::Normal,
-                '\\' => if let Some(next) = chars.next() {
-                    arg.push(next);
-                },
-                _ => arg.push(c),
-            }
-        }
-        last = c;
-    }
-    if arg != "" {
-        args.push(arg);
-    }
-    if args.is_empty() {
-       return (Cmd::Unknown(String::new()), args)
-    }
-
-    let command = args.remove(0);
-    let command = Cmd::resolve(&command);
-    (command, args)
 }
 
 fn eval(command: &Cmd, args: &Vec<String>, buffers: &mut Buffers) {
@@ -169,62 +90,6 @@ fn run_external(cmd: &Executable, args: &Vec<String>, buffers: &mut Buffers) {
         },
         Err(_) => (),
     }
-}
-
-enum Cmd {
-    Builtin(Builtin),
-    External(Executable),
-    Unknown(String),
-}
-
-enum Builtin {
-    Exit,
-    Echo,
-    Type,
-    Pwd,
-    Cd,
-}
-
-struct Executable {
-    name: String,
-    path: PathBuf,
-}
-
-impl Cmd {
-    fn resolve(command: &str) -> Self {
-        match command {
-            "exit" => Cmd::Builtin(Builtin::Exit),
-            "echo" => Cmd::Builtin(Builtin::Echo),
-            "type" => Cmd::Builtin(Builtin::Type),
-            "pwd" => Cmd::Builtin(Builtin::Pwd),
-            "cd" => Cmd::Builtin(Builtin::Cd),
-            _ => match cmd_from_path(command) {
-                Option::None => Cmd::Unknown(command.to_string()),
-                Some(data) => Cmd::External(data),
-            }
-        }
-    }
-}
-
-fn cmd_from_path(command: &str) -> Option<Executable> {
-    let path = env::var("PATH").unwrap();
-    let mut paths = env::split_paths(&path);
-    while let Some(path) = paths.next() {
-        let full_path = path.join(&command);
-        if full_path.is_file() {
-            if let Ok(metadata) = fs::metadata(&full_path) {
-                if metadata.permissions().mode() & 0o111 != 0 {
-                    return Some(
-                        Executable {
-                            name: command.to_string(),
-                            path: full_path,
-                        }
-                    )
-                }
-            }
-        }
-    }
-    None
 }
 
 
---
+++ src/parser.rs
@@ -0,0 +1,142 @@
+use std::path::PathBuf;
+use std::env;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
+
+
+enum ParseMode {
+    Normal,
+    SingleQuote,
+    DoubleQuote,
+}
+
+
+pub fn parse_command(command: &str) -> (Cmd, Vec<String>) {
+    let home = std::env::home_dir()
+        .map(|path| path.to_string_lossy().into_owned())
+        .unwrap_or_default();
+
+    let mut args: Vec<String> = Vec::new();
+    let mut last = ' ';
+    let mut arg = String::with_capacity(command.len());
+    let mut chars = command.trim().chars().peekable();
+    let mut mode = ParseMode::Normal;
+
+    while let Some(c) = chars.next() {
+        match mode {
+            ParseMode::Normal => match c {
+                ' ' => match last {
+                    ' ' => (),
+                    _ => {
+                        if !arg.is_empty() {
+                            args.push(arg);
+                            arg = String::new();
+                        }
+                    }
+                },
+                '\'' => mode = ParseMode::SingleQuote,
+                '"' => mode = ParseMode::DoubleQuote,
+                '\\' => if let Some(next) = chars.next() {
+                    arg.push(next);
+                },
+                '~' => arg.push_str(&home),
+                '>' => {
+                    let have_io_number =  arg == "1" || arg == "2";
+                    if !have_io_number && !arg.is_empty() {
+                        args.push(arg);
+                        arg = String::with_capacity(command.len());
+                    }
+                    arg.push(c);
+                    if let Some(&next) = chars.peek() {
+                        if next == '>' {
+                            chars.next();
+                            arg.push(c);
+                        }
+                    }
+                    args.push(arg);
+                    arg = String::with_capacity(command.len());
+                },
+                _ => arg.push(c),
+            },
+            ParseMode::SingleQuote => match c {
+                '\'' => mode = ParseMode::Normal,
+                _ => arg.push(c),
+            }
+            ParseMode::DoubleQuote => match c {
+                '"' => mode = ParseMode::Normal,
+                '\\' => if let Some(next) = chars.next() {
+                    arg.push(next);
+                },
+                _ => arg.push(c),
+            }
+        }
+        last = c;
+    }
+    if arg != "" {
+        args.push(arg);
+    }
+    if args.is_empty() {
+       return (Cmd::Unknown(String::new()), args)
+    }
+
+    let command = args.remove(0);
+    let command = Cmd::resolve(&command);
+    (command, args)
+}
+
+
+pub enum Cmd {
+    Builtin(Builtin),
+    External(Executable),
+    Unknown(String),
+}
+
+pub enum Builtin {
+    Exit,
+    Echo,
+    Type,
+    Pwd,
+    Cd,
+}
+
+pub struct Executable {
+    pub name: String,
+    pub path: PathBuf,
+}
+
+impl Cmd {
+    pub fn resolve(command: &str) -> Self {
+        match command {
+            "exit" => Cmd::Builtin(Builtin::Exit),
+            "echo" => Cmd::Builtin(Builtin::Echo),
+            "type" => Cmd::Builtin(Builtin::Type),
+            "pwd" => Cmd::Builtin(Builtin::Pwd),
+            "cd" => Cmd::Builtin(Builtin::Cd),
+            _ => match cmd_from_path(command) {
+                Option::None => Cmd::Unknown(command.to_string()),
+                Some(data) => Cmd::External(data),
+            }
+        }
+    }
+}
+
+fn cmd_from_path(command: &str) -> Option<Executable> {
+    let path = env::var("PATH").unwrap();
+    let mut paths = env::split_paths(&path);
+    while let Some(path) = paths.next() {
+        let full_path = path.join(&command);
+        if full_path.is_file() {
+            if let Ok(metadata) = fs::metadata(&full_path) {
+                if metadata.permissions().mode() & 0o111 != 0 {
+                    return Some(
+                        Executable {
+                            name: command.to_string(),
+                            path: full_path,
+                        }
+                    )
+                }
+            }
+        }
+    }
+    None
+}
