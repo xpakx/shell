534eb4ad0b0ede26f3b1ea9385f0527d199af417
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-15 23:36:36 +0200

    feat: add simple option to apply env exports to command

--- src/env_custom.rs
+++ src/env_custom.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, process::Command};
 
 struct EnvEntry {
     value: String,
@@ -25,9 +25,7 @@ pub fn create_env() -> Env {
 
 impl Env {
     pub fn get(&self, key: &str) -> Option<String> {
-        self.vars.get(key)
-            .or_else(|| self.vars.get(key))
-            .map(|v| String::from(&v.value))
+        self.vars.get(key).map(|v| String::from(&v.value))
     }
 
     pub fn set_var(&mut self, key: String, value: &str) {
@@ -56,5 +54,17 @@ impl Env {
         // TODO: better fallback
         self.get("HOME")
             .unwrap_or(String::from("/"))
+    }
+
+    pub fn apply_to_command(&self, cmd: &mut Command) {
+        // cmd.env_clear(); // ????
+
+        let exported_vars: Vec<(&String, &String)> = self.vars
+            .iter()
+            .filter(|(_, entry)| entry.exported)
+            .map(|(key, entry)| (key, &entry.value))
+            .collect();
+
+        cmd.envs(exported_vars);
     }
 }
