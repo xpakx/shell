7596a33bba4649dd1602d9c501670d8ea4131d6d
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-18 20:28:03 +0200

    test: add more tests

--- shelltest/main.py
+++ shelltest/main.py
@@ -1,27 +1,20 @@
 from unittest import TestCase, TestResult, TestLoader, TextTestRunner
 from functools import wraps
+from enum import IntEnum
 import pexpect
 
 SHELL_PATH = "../target/debug/shell"
 
 
-def ensure_exceptions(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except pexpect.TIMEOUT:
-            print("timeout")
-            self.fail("timed out")
-        except pexpect.EOF:
-            print("eof")
-            self.fail("shell exited")
-    return wrapper
+class ExpectResult(IntEnum):
+    SUCCESS = 0
+    TIMEOUT = 1
+    EOF = 2
 
 
 class TestShellSuite(TestCase):
     def setUp(self):
-        print(self._testMethodName)
+        print(self._testMethodDoc)
         self.shell = pexpect.spawn(
                 SHELL_PATH,
                 encoding="utf-8",
@@ -32,15 +25,34 @@ class TestShellSuite(TestCase):
         if self.shell.isalive():
             self.shell.terminate(force=True)
 
-    @ensure_exceptions
-    def test_suite(self):
+    def expect_exact(self, text: str) -> ExpectResult:
+        index = self.shell.expect_exact([text, pexpect.TIMEOUT, pexpect.EOF])
+        return ExpectResult(index)
+
+    def verify_result(self, result: ExpectResult, on_succes: str):
+        if result == ExpectResult.SUCCESS:
+            print(on_succes)
+        elif result == ExpectResult.TIMEOUT:
+            print("timeout")
+            self.fail("timed out")
+        elif result == ExpectResult.EOF:
+            print("eof")
+            self.fail("shell exited")
+
+    def test_initial_prompt(self):
         """Verify the shell prints prompt upon starting"""
+        result = self.expect_exact("$ ")
+        self.verify_result(result, "Initial prompt detected")
+
+    def test_invalid_command_error(self):
+        """Verify typing an unknown command outputs the error"""
         self.shell.expect_exact("$ ")
-        print("Initial prompt detected")
+        self.shell.sendline("invalid_command")
+        result = self.expect_exact("invalid_command: command not found")
+        self.verify_result(result, "Received expected error")
 
 
 class QuietTestResult(TestResult):
-
     def printErrors(self):
         pass
 
